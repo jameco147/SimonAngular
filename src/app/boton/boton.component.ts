610e93b2d1f109858d6a17d5dbab94e1958357d6
Eugenio Domenech <halleck.g.florida@gmail.com>
2019-01-18 19:47:20 +0100

    Ahora con soniditos!!!

--- src/app/boton/boton.component.ts
+++ src/app/boton/boton.component.ts
@@ -9,96 +9,99 @@ export class BotonComponent implements OnInit {
   index: number = -1;
   disabled: boolean = false;
   // piezaArray: number[] = [1,2,3,4,5,6];
-  piezaArray: number[] = [6,6,6,6,6,6];
+  piezaArray: number[] = [6, 6, 6, 6, 6, 6];
   arraySimmon: number[] = [];
   usados: number = 0;
   url: number = 1;
   arrayRespuestaUsuario: number[] = [];
-  estadoBoton : boolean = false;
-  turnoJugador : boolean = false;
+  estadoBoton: boolean = false;
+  turnoJugador: boolean = false;
   continuar: boolean = false;
+
 
 
   constructor() { }
 
   ngOnInit() {
     //pinta la secuencia
-    setInterval( ()=>{
-      this.piezaArray = [6,6,6,6,6,6];
+    setInterval(() => {
+      this.piezaArray = [6, 6, 6, 6, 6, 6];
       this.piezaArray[this.arraySimmon[this.index]] = 6;
-      this.index = (this.index+1);
+      this.index = (this.index + 1);
       this.piezaArray[this.arraySimmon[this.index]] = this.arraySimmon[this.index];
+      this.reproducir(this.arraySimmon[this.index]);
       if (this.index > this.arraySimmon.length) {
         this.turnoJugador = true;
       }
       console.log(this.index);
-    } , 1000);
+    }, 1000);
   }
 
-respuestaUsuario(num){
+  respuestaUsuario(num) {
+    this.reproducir(num);
+    if (this.turnoJugador) {
+      this.arrayRespuestaUsuario.push(num);
+      this.compararRespuesta();
 
-  if (this.turnoJugador) {
-    this.arrayRespuestaUsuario.push(num);
-    this.compararRespuesta();
-    
-    console.log(this.arraySimmon);
-    
-    console.log(this.arrayRespuestaUsuario);
-  
+      console.log(this.arraySimmon);
 
-    //pinta y despinta botones al hacer click
-    if (this.estadoBoton) {
-      this.piezaArray[num]=num;
-      this.usados = num;
-      this.estadoBoton = true;
-    } else {
-      this.piezaArray[this.usados]=6;
-      this.piezaArray[num]=num;
-      this.estadoBoton = false;
-      this.usados = num;
+      console.log(this.arrayRespuestaUsuario);
+
+
+      //pinta y despinta botones al hacer click
+      if (this.estadoBoton) {
+        this.piezaArray[num] = num;
+        this.usados = num;
+        this.estadoBoton = true;
+      } else {
+        this.piezaArray[this.usados] = 6;
+        this.piezaArray[num] = num;
+        this.estadoBoton = false;
+        this.usados = num;
+      }
     }
-  }
 
-  console.log(this.continuar + " continua?");
-  
+    console.log(this.continuar + " continua?");
 
-  if (this.continuar) {
-    this.arrayRespuestaUsuario = [];
-    this.start();
-    console.log("hola");
-    
-  }
 
-}
-
-start(){
-  this.arraySimmon.push(Math.floor(Math.random() * 6))
-  this.index = -1;
-}
-
-compararRespuesta(){
-    
-  for (let i = 0; i < this.arraySimmon.length; i++) {
-    if (this.arrayRespuestaUsuario[i] === this.arraySimmon[i]) {
-      console.log("Correcto");
-      this.continuar = true;
-      console.log(i + " este es i");
-      //this.arrayRespuestaUsuario = [];
-    } else if (this.arrayRespuestaUsuario[i] !== this.arraySimmon[i] && this.arrayRespuestaUsuario[i] !== undefined){
-      console.log("Incorrecto");
-      this.continuar = false;
-    } else {
-      this.continuar = false;
+    if (this.continuar) {
+      this.arrayRespuestaUsuario = [];
+      this.start();
+      console.log("hola");
     }
+
   }
 
-  
+  start() {
+    this.arraySimmon.push(Math.floor(Math.random() * 6))
+    this.index = -1;
+  }
 
-  
-  
+  compararRespuesta() {
+
+    for (let i = 0; i < this.arraySimmon.length; i++) {
+      if (this.arrayRespuestaUsuario[i] === this.arraySimmon[i]) {
+        console.log("Correcto");
+        this.continuar = true;
+        console.log(i + " este es i");
+        //this.arrayRespuestaUsuario = [];
+      } else if (this.arrayRespuestaUsuario[i] !== this.arraySimmon[i] && this.arrayRespuestaUsuario[i] !== undefined) {
+        console.log("Incorrecto");
+        this.continuar = false;
+      } else {
+        this.continuar = false;
+      }
+    }
+
+
+
+
+
+  }
+  reproducir(num) {
+    const audio = new Audio("../../assets/Bleeps/"+num+".wav");
+    audio.play();
 }
-
-
 
 }
 
@@ -144,7 +147,7 @@ export class BotonComponent {
   start() {
 
 
-    
+
     if (this.estadoInicio) {
       console.log('hola1');
       this.estadoInicio = false;
